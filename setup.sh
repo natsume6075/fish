2de54fd050a614c471e107cc82eb7791759053ba
natsume6075 <natsume6075@gmail.com>
2022-02-13 19:09:03 +0900

    シンボリックリンクの元を、スクリプトの配置ディレクトリから取るようにした

--- setup.sh
+++ setup.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-if test "$DROPBOX" = "" ; then
-    echo "Dropboxへのパスを環境変数 DROPBOX に格納してください。"
-    echo "bash: export DROPBOX=[pass]"
-    exit
-else
-    mkdir -p ~/.config/
-    ln -sfv $DROPBOX/fish ~/.config/fish
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+echo $SCRIPT_DIR
 
-    # oh my fish
-    curl -L http://get.oh-my.fish | fish
+mkdir -p ~/.config/
+ln -sfv $SCRIPT_DIR ~/.config/fish
 
-    # theme 変更
-    omf install agnoster
-    omf theme agnoster
-fi
+# oh my fish
+curl -L http://get.oh-my.fish | fish
+
+# theme 変更
+omf install agnoster
+omf theme agnoster
 
