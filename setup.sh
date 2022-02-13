783211907d20638757508e597243bae991040064
natsume6075 <natsume6075@gmail.com>
2022-02-13 18:31:55 +0900

    テーマ変更

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ else
     mkdir -p ~/.config/
     ln -sfv $DROPBOX/fish ~/.config/fish
 
+    # oh my fish
     curl -L http://get.oh-my.fish | fish
+
+    # theme 変更
+    omf install agnoster
+    omf theme agnoster
 fi
 
