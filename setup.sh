18a7a531bf3051b271b52fde9d748041b6b5c8e0
natsume6075 <natsume6075@gmail.com>
2022-02-13 19:18:36 +0900

    シンボリックリンクの張り先のミスを修正

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo $SCRIPT_DIR
 
 mkdir -p ~/.config/
-ln -sfv $SCRIPT_DIR ~/.config/fish
+ln -sfv $SCRIPT_DIR ~/.config/
 
 # oh my fish
 curl -L http://get.oh-my.fish | fish
