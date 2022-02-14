c420a9fd6f1644f6d5d4a73ccd458ffd7bc09517
natsume6075 <natsume6075@gmail.com>
2022-02-14 16:11:33 +0900

    AGL656950のローカル設定を整理

--- conf.d/entrypoint_local.fish
+++ conf.d/entrypoint_local.fish
@@ -1,7 +1,12 @@
+set filename (basename (status -f))
+
 if test "$NAME" = "DESKTOP-IVL0EDA"
-    set filename (basename (status -f))
     echo "$filename : set local settings for type $NAME"
-    source ~/.config/fish/conf.d/local/DESKTOP-IVL0EDA/*
+    source ~/.config/fish/conf.d/local/DESKTOP-IVL0EDA/env_var.fish
+else if test "$NAME" = "AGL656950"
+    echo "$filename : set local settings for type $NAME"
+    source ~/.config/fish/conf.d/local/AGL656950/alias.fish
+    source ~/.config/fish/conf.d/local/AGL656950/env_var.fish
 else
     echo "$NAME doesn't have local settings"
 end
