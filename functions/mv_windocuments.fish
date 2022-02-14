ea16d777fcd20cf435b5d829f66f1d579c347ef7
natsume6075 <natsume6075@gmail.com>
2022-02-14 16:14:10 +0900

    $WIN_DOCUMENTS -> $WIN_HOME/Documents

--- functions/mv_windocuments.fish
+++ functions/mv_windocuments.fish
@@ -1,4 +1,4 @@
-function mv_windocuments --description 'mv all args to $WIN_DOCUMENTS & open explorer'
-    mv $argv $WIN_DOCUMENTS
-    explorer.exe $WIN_DOCUMENTS
+function mv_windocuments --description 'mv all args to Documents & open explorer'
+    mv $argv $WIN_HOME/Documents
+    explorer.exe $WIN_HOME/Documents
 end
