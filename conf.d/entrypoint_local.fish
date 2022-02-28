404088d8b616debd333080eadbcee8e0b40e684c
natsume6075 <natsume6075@gmail.com>
2022-02-28 09:48:35 +0900

    シェルログイン時の echo を削除、previm の URL になぜか echo が含まれてしまうため

--- conf.d/entrypoint_local.fish
+++ conf.d/entrypoint_local.fish
@@ -1,13 +1,15 @@
 set filename (basename (status -f))
 
 if test "$NAME" = "DESKTOP-IVL0EDA"
-    echo "$filename : set local settings for type $NAME"
+    # echo "$filename : set local settings for type $NAME"
     source ~/.config/fish/conf.d/local/DESKTOP-IVL0EDA/env_var.fish
 else if test "$NAME" = "AGL656950"
-    echo "$filename : set local settings for type $NAME"
+    # echo "$filename : set local settings for type $NAME"
     source ~/.config/fish/conf.d/local/AGL656950/alias.fish
     source ~/.config/fish/conf.d/local/AGL656950/env_var.fish
 else
-    echo "$NAME doesn't have local settings"
+    # echo "$NAME doesn't have local settings"
 end
+
+# 暫定的に echo を消去。previm の URL になぜか含まれてしまうので。これ直してプルリク送りたいね。
 
