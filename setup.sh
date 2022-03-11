be78070f18afd340cf1daa6896b6f24b476eefde
natsume6075 <natsume6075@gmail.com>
2022-03-11 23:49:54 +0900

    omfインストールを対話型に

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo $SCRIPT_DIR
 
 mkdir -p ~/.config/
-
 ln -sfv $SCRIPT_DIR ~/.config
 
-
-# oh my fish
-curl -L http://get.oh-my.fish | fish
-
-# theme 変更
-echo ""
-echo "if you want to change theme, type: omf install [theme_name(ex.pure)]"
+echo "Install oh-my-fish?(y/[other])"
+read input
+if [ $input = 'y' ] ; then
+    curl -L http://get.oh-my.fish | fish
+    echo "If you want to change theme, type: omf install [theme_name(ex.pure)]"
+fi
 
