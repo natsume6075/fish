cb937ac26f4b13d582698cb55bbb1346e03888e2
natsume6075 <natsume6075@gmail.com>
2022-03-12 19:00:35 +0900

    install を分離

--- setup.sh
+++ setup.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+function GetConfirmation() {
+    echo "$1 (y/n)"
+    read input
+    if [ "$input" = 'y' ] ; then
+        return 1
+    elif [ "$input" = 'n' ] ; then
+        return 0
+    else
+        GetConfirmation "$1"
+    fi
+}
+
+
 mkdir -p ~/.config/
 ln -sfv $SCRIPT_DIR ~/.config/
 
-echo "Install oh-my-fish? (y/[other])"
-read input
-if [ $input = 'y' ] ; then
+# If necessary, install oh-my-fish.
+if ! GetConfirmation "Install oh-my-fish?" ; then
     curl -L http://get.oh-my.fish | fish
     echo "If you want to change theme, type: omf install [theme_name(ex.pure)]"
 fi
